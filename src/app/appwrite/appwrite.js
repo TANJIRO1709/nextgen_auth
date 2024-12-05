c4abbe582a0a1c374b55d63a98a570f751ab78a7
faker581 <hrushikeshsarangi7@gmail.com>
2024-12-06 02:24:55 +0530

    FInal Commit for appwrite backend

--- src/app/appwrite/appwrite.js
+++ src/app/appwrite/appwrite.js
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.PROJECT_ID); 
+    .setProject(`${process.env.PROJECT_ID}`); 
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
