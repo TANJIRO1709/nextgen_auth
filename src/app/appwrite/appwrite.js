10a168efccdf9b230442f9fdfde58f87dd8b1e91
faker581 <hrushikeshsarangi7@gmail.com>
2024-12-06 02:36:29 +0530

    Final Commit for appwrite

--- src/app/appwrite/appwrite.js
+++ src/app/appwrite/appwrite.js
@@ -1,10 +1,16 @@
-import { Client, Account } from 'appwrite';
+import { Client, Account, ID } from 'appwrite';
+
+const projectId = process.env.PROJECT_ID;
+
+if (!projectId) {
+  throw new Error('PROJECT_ID environment variable is not defined');
+}
 
 export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(`${process.env.PROJECT_ID}`); 
+    .setProject(projectId);
 
 export const account = new Account(client);
-export { ID } from 'appwrite';
+export { ID };
