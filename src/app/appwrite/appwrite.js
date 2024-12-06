819a7ea7a465d990a10db7c9f2ace7d4c86ca58b
faker581 <hrushikeshsarangi7@gmail.com>
2024-12-06 22:05:47 +0530

    Update

--- src/app/appwrite/appwrite.js
+++ src/app/appwrite/appwrite.js
@@ -1,6 +1,6 @@
 import { Client, Account, ID } from 'appwrite';
 
-const projectId = process.env.PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
 
 if (!projectId) {
   throw new Error('PROJECT_ID environment variable is not defined');
